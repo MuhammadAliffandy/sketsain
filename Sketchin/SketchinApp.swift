1947e17cf12e7dacc324b1215b85456c91cb3822
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2026-04-10 14:48:42 +0700

    Initial Commit

---
+++ Sketchin/SketchinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SketchinApp.swift
+//  Sketchin
+//
+//  Created by Muhammad Aliffandy on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SketchinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
